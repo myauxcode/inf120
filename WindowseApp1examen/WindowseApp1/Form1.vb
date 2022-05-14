9f10edf7733cd626b4553418ebbf9508a1ef13d6
myauxcode <104797580+myauxcode@users.noreply.github.com>
2022-05-14 09:05:04 -0400

    Add files via upload

---
+++ WindowseApp1examen/WindowseApp1/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+
+    Dim v1, v2, v3 As Vector
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        v1 = New Vector
+        v2 = New Vector
+        v3 = New Vector
+    End Sub
+    Private Sub CARGARToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CARGARToolStripMenuItem.Click
+        v1.cargarmanual(TextBox1.Text)
+        TextBox4.Text = v1.descargar
+    End Sub
+
+    Private Sub CARGARRNDToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CARGARRNDToolStripMenuItem.Click
+        v1.cargar(TextBox1.Text, TextBox2.Text, TextBox3.Text)
+        TextBox4.Text = v1.descargar
+    End Sub
+
+    Private Sub DESCARGARToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DESCARGARToolStripMenuItem.Click
+        TextBox4.Text = v1.descargar
+    End Sub
+
+    Private Sub PREGUNTA1ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PREGUNTA1ToolStripMenuItem.Click
+        TextBox5.Text = v1.ejercicio1
+    End Sub
+
+
+
+
+
+
+End Class
---
+++ WindowseApp1examen/WindowseApp1/Vector.vb
@@ -0,0 +1,55 @@
+﻿Public Class Vector
+    Const max = 100
+    Private v(max) As Integer
+    Private n As Integer
+    Public Sub Vector()
+        n = 0
+    End Sub
+    Public Sub cargar(ne As Integer, a As Integer, b As Integer)
+        n = ne
+        For i = 1 To n
+            v(i) = a + Rnd() * (b - a)
+        Next
+    End Sub
+    Public Sub cargarmanual(ne As Integer)
+        n = ne
+        For i = 1 To n
+            v(i) = InputBox("introducir un numero")
+        Next
+    End Sub
+    Public Function descargar()
+        Dim s As String = ""
+        For i = 1 To n
+            s = s + Str(v(i)) + ","
+        Next
+        Return s
+    End Function
+    Public Sub cargaru(ele As Integer)
+        n = n + 1
+        v(n) = ele
+    End Sub
+    Public Function frecuencia(ele As Integer)
+        Dim c = 0
+        For i = 1 To n
+            If (ele = v(i)) Then
+                c = c + 1
+            End If
+        Next
+        Return c
+    End Function
+    Public Function ejercicio1()
+        Dim primos As Vector = New Vector
+        Dim ne As Nent = New Nent
+        For i = 1 To n
+            ne.cargar(v(i))
+            If (ne.verifprimo And primos.frecuencia(ne.descargar) = 0) Then
+                primos.cargaru(ne.descargar)
+            End If
+        Next
+        Return primos.n
+    End Function
+
+
+
+
+End Class
